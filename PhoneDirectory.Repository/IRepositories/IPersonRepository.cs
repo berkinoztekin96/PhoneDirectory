c7d51d4466498fa73e0c64b762a2625323d62b07
Berkin Öztekin <berkin-oztekin@hotmail.com>
2021-09-18 00:03:35 +0300

    IRepository Generic Class added

---
+++ PhoneDirectory.Repository/IRepositories/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using PhoneDirectory.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectory.Repository.IRepositories
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+    }
+}
---
+++ PhoneDirectory.Repository/IRepositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.Repository.IRepositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity); 
+        void Delete(int id);
+      
+    }
+}
